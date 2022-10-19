26cff21b7ab6f1f0f7941a056e2bc84a8d5505f0
SergioA788 <106343291+SergioA788@users.noreply.github.com>
2022-10-19 06:55:14 -0500

    Delete py directory

--- py/factorial/factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(fact):
- factor = int(1)
- for s in range (1, fact + 1):
-     factor = factor * s
- return factor
-numero = int(input("ingrese un numero: "))
-print("el numero de la factorial es: " + str(factorial(numero)))
-      
--- py/suma/suma.py
+++
@@ -1,8 +0,0 @@
-def suma():
-    def operacion():
-        number1 = int(input("Primer numero: "))
-        number2 = int(input("Segundo numero: "))
-        d = number1 + number2
-        return (d)
-
-    print("La suma de los numeros son: ", operacion())
