ab3fc175bc8ed0fbe662c252741af5bad22696d6
SergioA788 <106343291+SergioA788@users.noreply.github.com>
2022-10-19 06:55:43 -0500

    Delete py directory

--- py/divicion/divicion.py
+++
@@ -1,6 +0,0 @@
-def divicion():
-    num1 = int(input("Primer numero: "))
-    num2 = int(input("Segundo numero: "))
-    resultado = num1 / num2
-    return resultado
-print("El resultado de la multiplicacion es: ", divicion());
